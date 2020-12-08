233b4afe8d009c1a5fb885261f7efeb8ade725a0
Your Name <you@example.com>
2020-12-07 22:46:48 -0800

    ///

--- install_i3.sh
+++ install_i3.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
+sudo /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2020.02.03_all.deb keyring.deb SHA256:c5dd35231930e3c8d6a9d9539c846023fe1a08e4b073ef0d2833acd815d80d48
+sudo dpkg -i ./keyring.deb
 
 if grep -q sur5r /etc/apt/sources.list.d/sur5r-i3.list; then
-    echo i3 already installed
+    echo sur5r already there
 else
-	sudo /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2020.02.03_all.deb keyring.deb SHA256:c5dd35231930e3c8d6a9d9539c846023fe1a08e4b073ef0d2833acd815d80d48
-	sudo dpkg -i ./keyring.deb
 	sudo echo "deb https://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
-	sudo apt update
-	sudo apt install i3
 fi
+sudo apt update
+sudo apt install i3
 
 
--- install_sublime.sh
+++ install_sublime.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+apt sudo-get install apt-transport-https
 if grep -q sublimetext /etc/apt/sources.list.d/sublime-text.list; then
-    echo sublime already installed
+    echo sublime source already there
 else
-	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-	apt sudo-get install apt-transport-https
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-	sudo apt-get update
-	sudo apt-get install sublime-text
-	mkdir -p  ~/.config/sublime-text-3
-	wget -P ~/.config/sublime-text-3/Installed\ Packages https://packagecontrol.io/Package%20Control.sublime-package
 fi
+sudo apt update
+sudo apt install i3
+
