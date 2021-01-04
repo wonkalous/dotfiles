f7fbc44ae4cf23b96f4972df9374a81f154f0ab1
Evan Penn <epenn@google.com>
2021-01-04 11:36:15 -0800

    calc

--- install_clipmenu.sh
+++ install_clipmenu.sh
@@ -2,7 +2,7 @@ cd ~/code
 git clone git@github.com:cdown/clipmenu.git
 git clone git@github.com:cdown/clipnotify.git
 
-sudo apt install gcc libx11-dev libxfixes-dev xsel
+sudo apt -y install gcc libx11-dev libxfixes-dev xsel
 cd clipnotify
 sudo make install
 cd clipmenu
--- install_rofi-calc.sh
+++ install_rofi-calc.sh
@@ -1,6 +1,6 @@
 cd ~/code
 git clone git@github.com:svenstaro/rofi-calc.git
-sudo apt install rofi-dev qalc libtool
+sudo apt -y install rofi-dev qalc libtool
 
 cd rofi-calc
 autoreconf -i
